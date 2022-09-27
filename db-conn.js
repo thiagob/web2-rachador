f9de89908675cb57711bac733e33eb7da0ed6a6d
thiagob <thiagob@gmail.com>
2022-09-27 00:44:46 -0300

    Adiciona demais métodos para tabela de eventos

--- db-conn.js
+++ db-conn.js
@@ -4,7 +4,13 @@ class DBConn {
 
     constructor() {
         this.db = new sqlite3.Database('db/dev.db');
+        this.createTables();
     }
+
+    getLastInsertRowId(callback) {
+        return this.db.get('SELECT last_insert_rowid()', callback);
+    }
+
 
     createTables() {
         var sql = `CREATE TABLE IF NOT EXISTS eventos (
@@ -13,13 +19,32 @@ class DBConn {
 
         return this.db.run(sql);
     }
-    
-    findAllEventos(callback) {
 
-        var sql = "SELECT * FROM eventos";
-        return this.db.all(sql, [], callback);
-
+    createEvento(nome, callback) {
+        var sql = 'INSERT INTO eventos (nome) VALUES (?)';
+        return this.db.run(sql, [nome], callback);
     }
+
+    updateEvento(id, nome, callback) {
+        var sql = 'UPDATE eventos SET nome = (?) WHERE ID = (?)';
+        return this.db.run(sql, [nome, id], callback);
+    }
+
+    GetEventoById(id, callback) {
+        var sql = 'SELECT * FROM eventos WHERE ID = (?)';
+        return this.db.get(sql, id, callback);
+    }    
+
+    findAllEventos(callback) {
+        var sql = 'SELECT * FROM eventos';
+        return this.db.all(sql, [], callback);
+    }
+
+    deleteEvento(id, callback) {
+        var sql = 'DELETE FROM eventos WHERE ID = (?)';
+        return this.db.run(sql, id, callback);
+    }    
+
 
 }
 
